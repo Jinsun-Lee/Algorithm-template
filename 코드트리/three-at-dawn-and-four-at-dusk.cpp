19d297a0600404e3ac89aa46ee6de61de02c9da0
Jinsun-Lee <68187536+Jinsun-Lee@users.noreply.github.com>
2025-04-21 23:56:23 +0900

    Title: 조삼모사, Time: 51ms, Memory: 0MB, Status: Passed - Codetree

--- 코드트리/three-at-dawn-and-four-at-dusk.cpp
+++ 코드트리/three-at-dawn-and-four-at-dusk.cpp
@@ -5,90 +5,50 @@
 using namespace std;
 #define endl "\n"
 
-/*
-n개의 일, 각각 n/2씩 조합 생성하고(N은 2의 배수)
-|아침(P12+P21) - 저녁(P34+P43)|가 최소가 되도록
+int N;               // 일의 양, N이 4~20
+int map[21][21];     // 각 값은 1~100
+vector<int> day, night; 
+vector<bool> used;
+int minDiff = INT_MAX;
 
-<헷갈>
-- 강도가 큰 건 상관없고, 차이만 최소가 되면 되려나?
-
-<수정>
-- 기준을 1부터로 잡을 필요가 없어, Pij=0만 주의
-*/
-
-/* <백트래킹 조건>
-
-i==j는 넘겨도 돼
-
-
-*/
-
-int N; // 일의 양, N이 4~20
-int map[21][21]; // 각 값은 1~100
-vector<int> day(11), night(11); // 낮밤 최대 10개씩으로 사이즈 제한
-int sumD, sumN, minDiff;
-
-int getSum(vector<int>& vec) {
+int getSum(const vector<int>& vec){
     int sum = 0;
     int len = vec.size();
     for (int i = 0; i < len; ++i) {
         for (int j = i + 1; j < len; ++j) {
-            sum += map[vec[i]][vec[j]] + map[vec[j]][vec[i]];
+            sum += map[vec[i]][vec[j]]; //+map[vec[j]][vec[i]];
         }
     }
     return sum;
 }
 
 void dfs(int depth, int start) {
-    if (depth >= N / 2) {
-        
-        // day에 없는 숫자들을 night에 추가
-        //for (int i = 0; i < N; ++i) {
-        //    if (!day[i]) night.push_back(i+1);
-        //}
 
-        // night 초기화
-        vector<bool> isDay(N, false);
-        for (int i : day) isDay[i] = true;
-        night.clear();
+    if (depth == N / 2) {
+        
+        // 이전 값 제거
+        day.clear(); 
+        night.clear(); 
+
+        // used 배열로 day와 night 팀을 나누기
         for (int i = 0; i < N; ++i) {
-            if (!isDay[i]) night.push_back(i);
+            if (used[i]) day.push_back(i);
+            else night.push_back(i);
         }
 
-        // day랑 night 각자 n/2개의 숫자가 있고, 그 숫자들에서 2개씩 뽑아 계산
-  
-        /*
-        //vector<bool> tmp(day.size(), true);
-        //for (int i = 0; i < day.size() - 2; ++i) tmp[i] = false;
-        int len = day.size(); vector<bool> tmp(len);
-        fill(tmp.end() - 2, tmp.end(), true);
-        do {
-            for (int i = 0; day.size(); ++i) {
-                if (tmp[i]) cout << day[i] << " ";
-            }
-            cout << endl;
-        } while (next_permutation(tmp.begin(), tmp.end()));
-        */
-
-        sumD = getSum(day);
-        sumN = getSum(night);
+        int sumD = getSum(day);
+        int sumN = getSum(night);
         minDiff = min(minDiff, abs(sumD - sumN));
-        
+
         return;
     }
-    
-    for (int i = start; i < N; ++i) {
-        day.push_back(i);
-        dfs(depth + 1, i+1);
-        day.pop_back();
-    }
-}
 
-void init() {
-    sumD = sumN = 0; 
-    minDiff = INT_MAX;
-    day.clear();
-    night.clear();
+    for (int i = start; i < N; ++i) {
+        used[i]=true;
+        dfs(depth + 1, i + 1);
+        used[i] = false;
+    }
+
 }
 
 int main() {
@@ -96,17 +56,23 @@ int main() {
 
     //freopen_s(new FILE*, "input.txt", "r", stdin); // 5, 19
 
-    init();
-
-    // 1. 입력 처리
+    // 1. 입력 처리, 누적합 사용
     cin >> N;
-    for (int i = 0; i < N; ++i) { 
+    
+    int tmp;
+    for (int i = 0; i < N; ++i) {
         for (int j = 0; j < N; ++j) {
-            cin >> map[i][j];
+            cin >> tmp;
+            if (i < j) map[i][j] = tmp;
+            else {
+                map[i][j] = 0;
+                map[j][i] += tmp;
+            }
         }
     }
-
+    
     // 2. N/2로 조합 생성
+    used.assign(N, false);
     dfs(0, 0);
 
     // 3. 조합 별로 강도 구하고, 최소값 저장
